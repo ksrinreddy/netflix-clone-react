8f159766487a67a91e5c113dcecc981fd3923deb
ksrinreddy <ksrinreddy@gmail.com>
2023-04-30 00:04:18 +0530

    structured error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section className="error">
+      <h2 className="error__response">404</h2>
+      <p className="error__message">Page not found</p>
+    </section>
+  );
 };
 
 export default Error;
