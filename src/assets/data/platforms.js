33a71c4e7c69a93625d2cde8372b1805e600126d
ksrinreddy <ksrinreddy@gmail.com>
2023-04-29 14:34:52 +0530

    structured platforms

---
+++ src/assets/data/platforms.js
@@ -0,0 +1,36 @@
+const platforms = [
+  {
+    id: 1,
+    title: "Enjoy on your TV.",
+    desc: "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    img: {
+      url: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/cards/v2.1/enjoyOnTv/en.png",
+    },
+  },
+  {
+    id: 2,
+    title: "Watch everywhere.",
+    desc: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+    img: {
+      url: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/cards/v2.1/watchEverywhere/en.png",
+    },
+  },
+  {
+    id: 3,
+    title: "Create profiles for children.",
+    desc: "Send children on adventures with their favourite characters in a space made just for them—free with your membership.",
+    img: {
+      url: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/cards/v2.1/kids/en-GB.png",
+    },
+  },
+  {
+    id: 4,
+    title: "Download your shows to watch offline.",
+    desc: "Save your favourites easily and always have something to watch.",
+    img: {
+      url: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/cards/v2.1/download/en.png",
+    },
+  },
+];
+
+export default platforms;
--- src/components/Platforms.js
+++ src/components/Platforms.js
@@ -1,7 +1,59 @@
 import React from "react";
+import platforms from "../assets/data/platforms";
 
 const Platforms = () => {
-  return <div>Platforms</div>;
+  const tv = platforms[0];
+  const streaming = platforms[1];
+  const children = platforms[2];
+  const download = platforms[3];
+  return (
+    <div className="platforms">
+      {/* tv */}
+      <article className="platform">
+        <div className="platform__info">
+          <h1 className="platform__title">{tv.title}</h1>
+          <p className="platform__desc">{tv.desc}</p>
+        </div>
+        <img src={tv.img?.url} alt="platform-img" className="platform__img" />
+      </article>
+      {/* streaming */}
+      <article className="platform">
+        <div className="platform__info order-2">
+          <h1 className="platform__title">{streaming.title}</h1>
+          <p className="platform__desc ">{streaming.desc}</p>
+        </div>
+        <img
+          src={streaming.img?.url}
+          alt="platform-img"
+          className="platform__img order-1"
+        />
+      </article>
+      {/* children */}
+      <article className="platform">
+        <div className="platform__info">
+          <h1 className="platform__title">{children.title}</h1>
+          <p className="platform__desc">{children.desc}</p>
+        </div>
+        <img
+          src={children.img?.url}
+          alt="platform-img"
+          className="platform__img"
+        />
+      </article>
+      {/* download */}
+      <article className="platform">
+        <div className="platform__info order-2">
+          <h1 className="platform__title">{download.title}</h1>
+          <p className="platform__desc ">{download.desc}</p>
+        </div>
+        <img
+          src={download.img?.url}
+          alt="platform-img"
+          className="platform__img order-1"
+        />
+      </article>
+    </div>
+  );
 };
 
 export default Platforms;
