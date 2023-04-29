150ec48c07f420bcd2654dc4cd670f2fd0e8eb3d
ksrinreddy <ksrinreddy@gmail.com>
2023-04-29 12:53:54 +0530

    structured footer

---
+++ src/assets/data/footerLinks.js
@@ -0,0 +1,79 @@
+const footerLinks = [
+  {
+    id: 1,
+    name: "FAQ",
+    url: "faq",
+  },
+  {
+    id: 2,
+    name: "Help Center",
+    url: "help-center",
+  },
+  {
+    id: 3,
+    name: "Account",
+    url: "account",
+  },
+  {
+    id: 4,
+    name: "Media Center",
+    url: "media-center",
+  },
+  {
+    id: 5,
+    name: "Investor Relations",
+    url: "investor-relations",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    url: "jobs",
+  },
+  {
+    id: 7,
+    name: "Ways to Watch",
+    url: "ways-to-watch",
+  },
+  {
+    id: 8,
+    name: "Terms of Use",
+    url: "terms-of-use",
+  },
+  {
+    id: 9,
+    name: "Privacy",
+    url: "privacy",
+  },
+  {
+    id: 10,
+    name: "Cookie Preferences",
+    url: "cookie-preferences",
+  },
+  {
+    id: 11,
+    name: "Corporate Information",
+    url: "corporate-information",
+  },
+  {
+    id: 12,
+    name: "Contact Us",
+    url: "contact-us",
+  },
+  {
+    id: 13,
+    name: "Speed Test",
+    url: "speed-test",
+  },
+  {
+    id: 14,
+    name: "Legal Notices",
+    url: "legal-notices",
+  },
+  {
+    id: 15,
+    name: "Only on Netflix",
+    url: "only-on-netflix",
+  },
+];
+
+export default footerLinks;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import footerLinksData from "../assets/data/footerLinks";
+import SelectLanguage from "./SelectLanguage";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [footerLinks, setFooterLinks] = useState(footerLinksData);
+  return (
+    <footer className="footer">
+      <p className="footer__tele-text">
+        Questions? Call{" "}
+        <Link to="tel:000-800-919-1694" className="footer__tele-no">
+          <span className="footer__tele-no-underline">000-800-919-1694</span>
+        </Link>
+      </p>
+      <div className="footer__links-container">
+        <ul className="footer__links">
+          {footerLinks.map((link) => {
+            const { id, name, url } = link;
+            return (
+              <li key={id} className="footer__list-item">
+                <Link to={`/${url}`} className="footer__link">
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <SelectLanguage />
+      <p className="footer__country">netflix india</p>
+    </footer>
+  );
 };
 
 export default Footer;
