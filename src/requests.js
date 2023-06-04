368e2bcd213ade77687fd0e6bbe275d10dd341eb
ksrinreddy <ksrinreddy@gmail.com>
2023-06-04 15:00:44 +0530

    trying to fix .env variable bug

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,5 @@
 const api_key = `${process.env.REACT_APP_API_KEY_TMDB}`;
-// requests
+// requests for different categories
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`,
